afb6221cb5b1f5314f7a91698efc3a37aebac72c
earamburo <edwin.aramburo1@gmail.com>
2025-09-29 18:10:31 -0400

    vite config deployment fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    base: mode === 'production' ? '/earamburo.github.io/' : '/',
+    base: '/',
     host: "::",
     port: 8080,
   },
