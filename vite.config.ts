28d91479797ef74906ff3cfbac1c4044e991e62f
earamburo <edwin.aramburo1@gmail.com>
2025-09-29 18:22:26 -0400

    config fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    base: '/',
+    base: '/portfolio',
     host: "::",
     port: 8080,
   },
